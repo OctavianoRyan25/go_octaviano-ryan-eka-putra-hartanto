14f3b7bd423b25842a2d58f2e5976c35348bd052
OctavianoRyan25 <octavianoryan25@gmail.com>
2024-02-24 20:55:08 +0700

    submit all task in week 1

--- 2_Basic Programming/praktikum/main.go
+++ 2_Basic Programming/praktikum/main.go
@@ -3,10 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
-	
+	//
+	for i := 1; i <= 5; i++ {
+		for j := 1; j <= i; j++ {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+	for i := 1; i <= 5; i++ {
+		for j := 5; j >= i; j-- {
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+	for i := 1; i <= 5; i++ {
+		for k :=4 ; k >= i; k-- {
+			fmt.Print(" ")
+		}
+		for j := 1; j <= i; j++ {
+			fmt.Print("* ")
+		}
+		fmt.Println()
+	}
 }
-
-
-
-
